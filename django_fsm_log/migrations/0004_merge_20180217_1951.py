100788c354a8ecbe40d70315daedc0ccbc7dd8d9
David Binetti <dbinetti@gmail.com>
2018-02-17 19:54:55 -0800

    Add migration merge

---
+++ django_fsm_log/migrations/0004_merge_20180217_1951.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('django_fsm_log', '0003_auto_20170612_1747'),
+        ('django_fsm_log', '0003_statelog_description'),
+    ]
+
+    operations = [
+    ]
